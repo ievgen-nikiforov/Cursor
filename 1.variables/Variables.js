550178762f476caad95e64a2ccd92dafd2832ab1
Ievgen Nikiforov <ievgen.nikiforov@gmail.co>
2021-02-19 21:24:14 +0200

    virables h/w

--- 1.variables/Variables.js
+++ 1.variables/Variables.js
@@ -2,5 +2,18 @@ let a=15.678;
 let b=123.965;
 let c=90.2345;
 // max value
-console.log(console.log(Math.max(a, b, c)));
-// 
+
+console.log("max value", (Math.max(a, b, c)));
+// min value
+console.log("min value",Math.min(a, b, c));
+// summ
+let summ=a+b+c;
+console.log("summ of all values is summ=",summ);
+// summ of rounded to the floow numbers
+let roundSummValue=Math.floor(a)+Math.floor(b)+Math.floor(c)
+console.log("summ of floor values",roundSummValue );
+
+console.log("round to the hundreds", Math.ceil(roundSummValue/100)*100);
+
+    
+
