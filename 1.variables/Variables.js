8fae7f6a7812122cec51ed52a4ebfeb61a5d698c
ievgen-nikiforov <ievgen.nikiforov@gmail.com>
2021-02-20 00:07:51 +0200

    finished

--- 1.variables/Variables.js
+++ 1.variables/Variables.js
@@ -15,5 +15,16 @@ console.log("summ of floor values",roundSummValue );
 
 console.log("round to the hundreds", Math.ceil(roundSummValue/100)*100);
 
-    
+console.log("boolean value if the floor summ is odd", Boolean(Math.floor(summ)%2));
+console.log("change amount of 500 minus summ of prices", 500 -summ);
+console.log("rounded avarage price", Math.round((summ/3)*100)/100);
+let discountPercent=Math.random();
+console.log("discount is", discountPercent);
+let realCost=summ/2;
+console.log("real Cost of the products is", realCost);
+let discountValue=summ*discountPercent;
+console.log("the value of discount", discountValue);
+reducedProfit=realCost-discountValue;
+console.log("the reduced profit after discount is", reducedProfit);
+console.log("price to pay rounded", (Math.round((summ-discountValue)*100))/100);
 
