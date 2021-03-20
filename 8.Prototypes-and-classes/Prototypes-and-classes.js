a5d8b998686a358540fa924ba688d5cdc419a5c4
ievgen-nikiforov <ievgen.nikiforov@gmail.com>
2021-03-21 00:24:24 +0200

    new features added

--- 8.Prototypes-and-classes/Prototypes-and-classes.js
+++ 8.Prototypes-and-classes/Prototypes-and-classes.js
@@ -5,14 +5,30 @@ class Student {
       this.fullName = fullName;   
     }
     //task 3 - marks
-    marks = [5, 4, 4, 5];
+    marks=[5, 4, 4, 5]
     get marks() {
-        this.marks=marks;
+        this.marks = marks;
         return this.marks;
       }
+       //set new mark
+    set setMarks (value){
+        this.marks.push(value);
+        return this.marks
     }
+    getAverageMark(){
+    const marks = this.marks;
+    const sum = marks.reduce(function(sum, current) {
+        return sum + current;
+      }, 0);
+      return this.avarageMark = sum / marks.length;
+
+    }
+    }
+
   
-  
-  const user = new Student("KPI", "4", "Test Testerok")
+  const user = new Student ("KPI", "4", "Test Testerok")
   console.log(user);
   console.log("Marks are ", user.marks)
+  user.setMarks = 5;
+  console.log("Marks are ", user.marks)
+  console.log("Marks are ", user.getAverageMark())
