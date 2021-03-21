69a74e2b5d354f37abfe7a503ac684c6e11ba2a0
ievgen-nikiforov <ievgen.nikiforov@gmail.com>
2021-03-21 15:35:01 +0200

    All required tasks are done

--- 8.Prototypes-and-classes/Prototypes-and-classes.js
+++ 8.Prototypes-and-classes/Prototypes-and-classes.js
@@ -4,6 +4,12 @@ class Student {
       this.course = course;
       this.fullName = fullName;   
     }
+
+    // task 2 - getInfo()
+    getInfo(){
+        const info = "Student of the "+this.course+" course in "+this.university+", "+this.fullName;
+        return info
+    }
     //task 3 - marks
     marks=[5, 4, 4, 5]
     get marks() {
@@ -14,7 +20,7 @@ class Student {
     set setMarks (value){
         this.marks.push(value);
         return this.marks
-    }
+        }
     getAverageMark(){
     const marks = this.marks;
     const sum = marks.reduce(function(sum, current) {
@@ -22,13 +28,27 @@ class Student {
       }, 0);
       return this.avarageMark = sum / marks.length;
 
+        }
+    dismiss(){
+        this.oldMarks=this.marks
+        this.marks = null;
+    }
+    recover(){
+
+        this.marks=this.oldMarks;
+        return "Student is recovered"
     }
     }
 
   
   const user = new Student ("KPI", "4", "Test Testerok")
-  console.log(user);
+  console.log( user.constructor);
+  console.log("getInfo >", user.getInfo())
   console.log("Marks are ", user.marks)
   user.setMarks = 5;
-  console.log("Marks are ", user.marks)
-  console.log("Marks are ", user.getAverageMark())
+  console.log("When 5 added, marks are ", user.marks)
+  console.log("Avarage mark is ", user.getAverageMark())
+  user.dismiss();
+  console.log("After user was dismissed, marks =", user.marks)
+  console.log (user.recover());
+  console.log("Marks which student had before dismissing ", user.marks)
