50a19a2f7667de15c8bb5ec64547236476e22342
ievgen-nikiforov <ievgen.nikiforov@gmail.com>
2021-03-21 15:52:38 +0200

    prettier

--- 8.Prototypes-and-classes/Prototypes-and-classes.js
+++ 8.Prototypes-and-classes/Prototypes-and-classes.js
@@ -2,53 +2,57 @@ class Student {
     constructor(university, course, fullName) {
       this.university = university;
       this.course = course;
-      this.fullName = fullName;   
+      this.fullName = fullName;
     }
-
+  
     // task 2 - getInfo()
-    getInfo(){
-        const info = "Student of the "+this.course+" course in "+this.university+", "+this.fullName;
-        return info
+    getInfo() {
+      const info =
+        "Student of the " +
+        this.course +
+        " course in " +
+        this.university +
+        ", " +
+        this.fullName;
+      return info;
     }
     //task 3 - marks
-    marks=[5, 4, 4, 5]
+    marks = [5, 4, 4, 5];
     get marks() {
-        this.marks = marks;
-        return this.marks;
-      }
-       //set new mark
-    set setMarks (value){
-        this.marks.push(value);
-        return this.marks
-        }
-    getAverageMark(){
-    const marks = this.marks;
-    const sum = marks.reduce(function(sum, current) {
+      this.marks = marks;
+      return this.marks;
+    }
+    //set new mark
+    set setMarks(value) {
+      this.marks.push(value);
+      return this.marks;
+    }
+    getAverageMark() {
+      const marks = this.marks;
+      const sum = marks.reduce(function (sum, current) {
         return sum + current;
       }, 0);
-      return this.avarageMark = sum / marks.length;
-
-        }
-    dismiss(){
-        this.oldMarks=this.marks
-        this.marks = null;
+      return (this.avarageMark = sum / marks.length);
     }
-    recover(){
-
-        this.marks=this.oldMarks;
-        return "Student is recovered"
+    dismiss() {
+      this.oldMarks = this.marks;
+      this.marks = null;
     }
+    recover() {
+      this.marks = this.oldMarks;
+      return "Student is recovered";
     }
-
+  }
   
-  const user = new Student ("KPI", "4", "Test Testerok")
-  console.log( user.constructor);
-  console.log("getInfo >", user.getInfo())
-  console.log("Marks are ", user.marks)
+  const user = new Student("KPI", "4", "Test Testerok");
+  console.log(user.constructor);
+  console.log("getInfo >", user.getInfo());
+  console.log("Marks are ", user.marks);
   user.setMarks = 5;
-  console.log("When 5 added, marks are ", user.marks)
-  console.log("Avarage mark is ", user.getAverageMark())
+  console.log("When 5 added, marks are ", user.marks);
+  console.log("Avarage mark is ", user.getAverageMark());
   user.dismiss();
-  console.log("After user was dismissed, marks =", user.marks)
-  console.log (user.recover());
-  console.log("Marks which student had before dismissing ", user.marks)
+  console.log("After user was dismissed, marks =", user.marks);
+  console.log(user.recover());
+  console.log("Marks which student had before dismissing ", user.marks);
+  
