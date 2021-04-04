687ea463ae92fbfd8cc62bbf8341d4a78d6089db
ievgen-nikiforov <ievgen.nikiforov@gmail.com>
2021-04-04 23:27:04 +0300

    generator

---
+++ 15.Generator/Generator.js
@@ -0,0 +1,15 @@
+
+function planetList(){
+   fetch('https://swapi.dev/api/planets')
+    .then((response) => {
+        return response.json();
+    })
+    .then((data) => {
+        const planetList = [];
+       data.results.map((p, i) => planetList.push(p.name))
+       const divPlanets = document.createElement('div')
+divPlanets.innerHTML = "<b>List of planets </b> <br> " + planetList;
+document.body.append(divPlanets);  
+});
+}
+planetList();
