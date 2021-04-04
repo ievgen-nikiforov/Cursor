1f04d839e30b2801906d3334bcb3aa87b6db244e
ievgen-nikiforov <ievgen.nikiforov@gmail.com>
2021-04-05 00:27:50 +0300

    generator

--- 15.Generator/Generator.js
+++ 15.Generator/Generator.js
@@ -1,15 +1,37 @@
 
-function planetList(){
-   fetch('https://swapi.dev/api/planets')
-    .then((response) => {
-        return response.json();
-    })
-    .then((data) => {
-        const planetList = [];
-       data.results.map((p, i) => planetList.push(p.name))
-       const divPlanets = document.createElement('div')
-divPlanets.innerHTML = "<b>List of planets </b> <br> " + planetList;
-document.body.append(divPlanets);  
-});
+console.log ("Task 1")
+function* createIdGenerator(){
+    let i = 0;
+ while (true){
+   i++;
+  yield i;
+ }
 }
-planetList();
+const idGenerator = createIdGenerator()
+console.log ("idGenerator.next().value >",idGenerator.next().value)
+console.log ("idGenerator.next().value >",idGenerator.next().value)
+console.log ("idGenerator.next().value >",idGenerator.next().value)
+console.log ("idGenerator.next().value >",idGenerator.next().value)
+
+console.log ("Task 2")
+function* newFontGenerator(font){
+while (true){
+    current =font;
+    let changeFont = yield current;
+    if (changeFont === "up"){
+        font = font+2;
+    }
+    if (changeFont === "down"){
+        font = font-2;
+    }
+    }
+}
+const fontGenerator = newFontGenerator(14);
+console.log(fontGenerator.next("up").value);
+console.log(fontGenerator.next("up").value);
+console.log(fontGenerator.next("up").value);
+console.log(fontGenerator.next().value);
+console.log(fontGenerator.next("down").value);
+console.log(fontGenerator.next("down").value);
+console.log(fontGenerator.next("down").value);
+console.log(fontGenerator.next().value);
